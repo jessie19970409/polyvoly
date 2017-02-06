8ae9ecd818ebcfbdf95544cda576d9f80a482452
jessie19970409 <yjiang04@risd.edu>
2017-02-06 09:59:25 -0500

    ok it will have a domain name soon

---
+++ Project4_porfolio/toggle.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+   $(".togglebutton").click(function(){
+    $(".timeline").slideToggle('slow');
+    });
+  
+});
+
+$(document).ready(function(){
+   $(".togglebutton").click(function(){
+    $(".comment").slideToggle('slow');
+    });
+  
+});
+
+$(document).ready(function(){
+   $(".togglemenu").click(function(){
+    $(".navigationmobile").slideToggle('slow');
+    });
+  
+});
+
+
